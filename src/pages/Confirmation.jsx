30342e16e3d0c8a6af6963436cd06e3f9257b0b3
owojutimilehin-sketch <owojutimilehin@gmail.com>
2026-06-01 14:31:39 +0100

    Add files via upload

---
+++ src/pages/Confirmation.jsx
@@ -0,0 +1,9 @@
+export default function Confirmation() {
+  return (
+    <div style={{ padding: "20px", textAlign: "center" }}>
+      <h1>🎉 Booking Successful!</h1>
+      <p>Your tour guide has been booked.</p>
+      <p>We’ll contact you with details soon.</p>
+    </div>
+  );
+}
